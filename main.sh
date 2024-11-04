95e705ea7e8abeba4d3e70e449dead1d59b5be96
shuubhh <bshubh27@gmail.com>
2024-11-04 21:05:39 +0530

    updated for api_gateway_url addition

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SERVICE_NAME="tf-ko-test01"
+SERVICE_NAME="tf-ko-run-service"
 YAML_FILE="spec.yaml"
-
+JS_FILE="script.js"
 #KO_DOCKER_REPO="asia-south1-docker.pkg.dev/civic-replica-421010/example"
 
 #fetch URL of Cloud Run service
@@ -27,6 +27,28 @@ while true; do
         break
     else
         echo "Waiting for Cloud Run service to provision..."
+        sleep 2
+    fi
+done
+
+get_api_gateway() {
+    GATEWAY_URL=$(gcloud api-gateway gateways describe apii-gateway --location "asia-northeast1" --format json | jq -r .defaultHostname)
+    if [ -n "$GATEWAY_URL" ]; then
+        echo "$GATEWAY_URL"
+    else
+        echo ""
+    fi
+}
+
+while true; do
+    GATEWAY_URL=$(get_api_gateway)
+    if [ -n "$GATEWAY_URL"]; then
+        ESC_GATEWAY_URL=$(echo $GATEWAY_URL | sed 's/[\/&]/\\&/g')
+        sed -i "s/API_GATEWAY_URL/$ESC_GATEWAY_URL/" $JS_FILE
+        echo "API Gateway URL added to $JS_FILE"
+        break
+    else
+        echo "Waiting for API Gateway to provision..."
         sleep 2
     fi
 done
